d9bc5ea544432542a1e7d65de1f08f9686e42b7b
Amir Aryani <amir.aryani@gmail.com>
2015-10-20 14:15:05 +1000

    Update to the about page

--- applications/portal/templates/omega/about.blade.php
+++ applications/portal/templates/omega/about.blade.php
@@ -26,67 +26,33 @@ $banner = isset($banner) ? $banner : asset_url('images/collection_banner.jpg', '
                         <div class="text-center" style="-webkit-animation: 0s;">
                             <h1 class="bigger hairline bordered bordered-normal os-animation animated fadeIn" data-os-animation="fadeIn" data-os-animation-delay="0s" style="-webkit-animation: 0s;color:#000000">
 
-                                A service built on sharing </h1>
+                                An Open Collaborative Software Project</h1>
                         </div>
                         <div style="padding-left:50px;padding-right:50px">
-                            <p>Research Data Switchboard helps you find, access, and reuse data for research from over one hundred
-                                Australian research organisations, government agencies, and cultural institutions.
-                                We do not store the data itself here but provide descriptions of, and links to, the data from our <a href="../contributors">data publishing partners</a>.
+                            <p>
+Research Data Switchboard (RD-Switchboard) is an open and collaborative software solution initiated by ANDS as part of a working group of the Research Data Alliance. The working group has input from:
+<ul>
+	<li>Australian National Data Service (ANDS) (Australia)</li>
+	<li>Dryad (US)</li>
+	<li>CERN InspireHEP (Switzerland)</li>
+	<li>figshare (UK)</li>
+	<li>da|ra and GESIS (Germany)</li>
+	<li>Data Curation Unit (Greece)</li></li>
+	<li>OpenAIRE (European Infrastructure)</li>
+</ul>
+
+<p>
+RD-Switchboard project addresses the problem of cross platform discovery by connecting datasets together across multiple registries on the basis of co-authorship or other collaboration models such as joint funding and grants. The best metaphor for these services is the SEE ALSO section in online bookstores, where customers are invited to look at other products by the same author, related topics or similar publishers. 
+</p>
+<p>
+This website (www.rd-switchboard.net) is a demonstrator of this capability data collections in Research Data Australia, Dryad and CERRN. In addition, it shows how <a href="https://github.com/rd-switchboard/Widgets">RD-Switchboard Widget and API</a> can be integrated into research data discovery platforms.
+</p>
                             </p>
                         </div>
                     </div>
-                    <div class="col-md-2 swatch-gray">  </div>
                 </div>
             </div>
 
-
-            <div class="col-md-12">
-                <div class="col-md-2 not-condensed os-animation animated fadeInUp"> </div>
-                <div class="col-md-10  ">
-                    <div class="col-md-4" >
-                        @include('includes/about-subjects-list')
-                    </div>
-                    <div class="col-md-6 text-center" style="padding-left:20px;padding-top:30px" >
-                        <h1 class="bigger hairline bordered bordered-normal os-animation animated fadeIn" data-os-animation="fadeIn" data-os-animation-delay="0s" style="-webkit-animation: 0s;color:#000000">Data for researchers everywhere</h1>
-                        <div class="text-left">
-                        <p>Research Data Switchboard caters specifically for researchers but also has broader relevance to others including policy makers, educators and business people.</p>
-                        <p>Research Data Switchboard covers a broad spectrum of research fields - across sciences, social sciences, arts and humanities. Much of the data you can discover
-                            here is immediately accessible online via our partners and free to use (subject to any licence conditions).</p>
-                            </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-12  swatch-gray data-os-animation-delay">
-                <div class="col-md-2 not-condensed os-animation animated fadeInUp swatch-gray data-os-animation-delay"> </div>
-                <div class="col-md-8 not-condensed os-animation animated fadeInUp swatch-gray data-os-animation-delay" style="-webkit-animation: 0s;padding-top:30px;padding-bottom:50px">
-                    <div style="-webkit-animation: 0s;padding-left:50px">
-                        <h1 class="bigger hairline bordered bordered-normal os-animation animated fadeIn" data-os-animation="fadeIn" data-os-animation-delay="0s" style="-webkit-animation: 0s;color:#000000">More than just a search engine</h>
-                    </div>
-                    <div style="padding-left:50px;padding-right:50px">
-                        <p>A single search in Research Data Switchboard retrieves data resources across a wide range of subjects and providers, so that you can: </p>
-                        <p>
-                        <ul  class="fa-ul features-list element-no-bottom fadeInLeft data-os-animation-delay" data-os-animation="fadeIn" data-os-animation-delay="0s" style="-webkit-animation: 0.4s;">
-                            <li class="element-no-top os-animation animated fadeInLeft data-os-animation-delay" >
-                                <div class="features-list-icon box-animate" style="background-color:#e9e9e9;"><i class="fa fa-recycle fa-6x" ></i></div><div style="padding-top:20px;font-weight: bold">reuse existing data</div>
-                            </li>
-                            <li class="element-no-top os-animation animated fadeInLeft data-os-animation-delay" >
-                                <div class="features-list-icon box-animate" style="background-color:#e9e9e9;"><i class="fa fa-binoculars fa-4x" ></i></div><div style="padding-top:20px;font-weight: bold"">explore beyond your discipline</div>
-                            </li>
-                            <li class="element-no-top os-animation animated fadeInLeft data-os-animation-delay">
-                                <div class="features-list-icon box-animate" style="background-color:#e9e9e9;"><i class="fa fa-puzzle-piece fa-4x" ></i></div><div style="padding-top:20px;font-weight: bold"">assemble data resources to solve big problems</div>
-                            </li>
-                         </ul>
-                        </p>
-                        <p>
-                            For example a search for the town "Wagga Wagga" will return results for data from a number of fields of research including:
-                            Earth Sciences, Agriculture and Veterinary sciences, Environmental Sciences, Built Environment and Design, Biological Sciences,
-                            Studies in Human Society, Studies in Creative Arts, History and Archaeology, and Philosophy and Religious Studies.
-                        </p>
-                    </div>
-                </div>
-                <div class="col-md-2 swatch-gray">  </div>
-            </div>
 
 
         <div class="col-md-12 swatch-black" style="background-image: url({{$banner}}); background-size: cover; background-repeat: no-repeat;" class="background-media skrollable skrollable-between">
@@ -108,56 +74,7 @@ $banner = isset($banner) ? $banner : asset_url('images/collection_banner.jpg', '
             <div class="col-md-2"> </div>
         </div>
 
-            <div class="col-md-12 swatch-gray" style="padding-bottom:-20px">
-                <div class="col-md-2 not-condensed os-animation animated fadeInUp swatch-gray"> </div>
-                <div class="col-md-8 not-condensed os-animation animated fadeInUp swatch-gray" style="-webkit-animation: 0s;padding-top:30px;padding-bottom:50px">
-                    <div class="text-center" style="-webkit-animation: 0s;">
-                        <h1 class="bigger hairline bordered bordered-normal os-animation animated fadeIn" data-os-animation="fadeIn" data-os-animation-delay="0s" style="-webkit-animation: 0s;color:#000000">Learn more</h>
-                    </div>
-                    <div class="row " data-os-animation="" data-os-animation-delay="">
-                        <div class="col-md-4 ">
-                            <a href="http://ands.org.au/researchers/" target="_blank">
-                                <div class="element-medium-top element-medium-bottom text-center os-animation animated fadeInUp" data-os-animation="fadeInUp" data-os-animation-delay=".0s" style="-webkit-animation: 0s;">
-                                    <div class="box box-round box-medium box-simple">
-                                        <div class="box-dummy"></div>
-                                        <div class="box-inner grid-overlay-0" style="background-color:#353b42;">
-                                            <i class="fa fa-share-alt" style="color:#ffffff"></i>
-                                        </div>
-                                    </div>
-                                    <p class="">Becoming a contributor</p>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-md-4 ">
-                            <a href="" data-toggle="modal" data-target="#help_modal" class="search_help">
-                                <div class="element-medium-top element-medium-bottom text-center os-animation animated fadeInUp" data-os-animation="fadeInUp" data-os-animation-delay=".3s" style="-webkit-animation: 0.3s;">
-                                    <div class="box box-round box-medium box-simple">
-                                        <div class="box-dummy"></div>
-                                        <div class="box-inner grid-overlay-0" style="background-color:#353b42;">
-                                            <i class="fa fa-search" style="color:#ffffff"></i>
-                                        </div>
-                                    </div>
-                                    <p class="">Searching for data</p>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-md-4 ">
-                            <a href="http://www.ands.org.au/discovery/reuse.html" target="_blank">
-                                <div class="element-medium-top element-medium-bottom text-center os-animation animated fadeInUp" data-os-animation="fadeInUp" data-os-animation-delay=".6s" style="-webkit-animation: 0.6s;">
-                                    <div class="box box-round box-medium box-simple">
-                                        <div class="box-dummy"></div>
-                                        <div class="box-inner grid-overlay-0" style="background-color:#353b42">
-                                            <i class="fa fa-external-link" style="color:#ffffff"></i>
-                                        </div>
-                                    </div>
-                                    <p class="">Accessing and reusing research data</p>
-                                </div>
-                            </a>
-                        </div>
 
-                    </div>
-                </div>
-            </div>
     </section>
 </article>
 @stop
